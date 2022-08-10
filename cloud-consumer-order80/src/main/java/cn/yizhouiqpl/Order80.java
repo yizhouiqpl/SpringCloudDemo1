fd6585f5a7f35185cccfe41b35a93c5efb3f6284
huangyufeng <huangyufeng@timingmar.com>
2022-08-10 21:41:00 +0800

    second commit;

--- cloud-consumer-order80/src/main/java/cn/yizhouiqpl/Order80.java
+++ cloud-consumer-order80/src/main/java/cn/yizhouiqpl/Order80.java
@@ -1,11 +1,17 @@
 package cn.yizhouiqpl;
 
+import cn.yizhouiqpl.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+/**
+ * 在启动该微服务的时候就能去加载我们的自定义Ribbon配置类，从而使配置生效，形如：
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration= MySelfRule.class)
 public class Order80 {
     public static void main(String[] args) {
         SpringApplication.run(Order80.class, args);
---
+++ cloud-providerconsul-payment8006/src/main/java/cn/yizhouiqpl/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package cn.yizhouiqpl.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("payment")
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("consul")
+    public String paymentInfo() {
+        return "springcloud with consul: "+serverPort+"\t\t"+ UUID.randomUUID();
+    }
+}
